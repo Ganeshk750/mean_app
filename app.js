a0514b77b40ca2fc7351e6354659c8ad94f33404
Ganeshk750 <ganesh9ot@gmail.com>
2020-05-12 20:08:16 +0530

    Registration & Logn Done

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cors = require('cors');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+
+require('./config/passport-config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var productRouter = require('./routes/product');
 
 var app = express();
 
@@ -16,13 +22,15 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/product', passport.authenticate('jwt', { session: false }), productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,3 +49,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
